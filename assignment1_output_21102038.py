29396fc124e8994650e1e12ce33502f3e9a14ed8
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-04-24 01:23:36 +0530

    Add files via upload

---
+++ assignment1_output_21102038.py
@@ -0,0 +1,28 @@
+PS D:\Python> python -u "d:\Python\assignment1.py"
+#Output of first question
+Enter 1st number: 26
+Enter 2nd number: 50
+Enter 3rd number: 45
+40.333333333333336
+#Output of second question
+Enter your income to the nearest penny: 25000
+Enter number of dependents: 2
+$ 1800.0
+#Output of third question
+Enter your Student ID: 21102038
+Enter your name: Raghav Goel
+Enter your gender: M for Male, F for Female, U for Unknown: M
+Enter your course: ES1101
+Enter your CGPA: 10
+['21102038', 'Raghav Goel', 'M', 'ES1101', 10.0]
+#Output of fourth question
+Enter marks of student 1: 68
+Enter marks of student 2: 79
+Enter marks of student 3: 93
+Enter marks of student 4: 49
+Enter marks of student 5: 90
+[49, 68, 79, 90, 93]
+#Output of fifth question
+Provided list of colors is : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List after modification is : ['Red', 'Green', 'White', 'Pink', 'Yellow']
+After replacing the color list is : ['Red', 'Green', 'White', 'Purple', 'Yellow']
