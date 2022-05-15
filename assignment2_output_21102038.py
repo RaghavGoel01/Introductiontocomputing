b0e60cfb0db22bdeeabfd0f9e106563e9d3c1809
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-05-15 21:24:28 +0530

    Add files via upload

---
+++ assignment2_output_21102038.py
@@ -0,0 +1,40 @@
+PS D:\Python> python -u "d:\Python\assignment2_21102038.py"
+
+#Output of first question
+36
+.egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language.
+10
+Pythonisacasesensitivelanguage.
+
+#Output of second question
+Enter your name: Raghav Goel
+Enter your SID: 21102038
+Enter your department name: Civil
+Enter your CGPA: 10
+Hey, Raghav Goel Here!
+My SID is 21102038
+I am from Civil department and my CGPA is 10
+
+#Output of third question
+8
+58
+50
+224
+40
+14
+0
+
+#Output of fourth question
+Enter your string: My name is Raghav Goel
+Yes
+
+#Output of fifth question
+Enter 1st side: 10
+Enter 2nd side: 12
+Enter 3rd side: 13
+Yes
+
+#Output of sixth question
+4
