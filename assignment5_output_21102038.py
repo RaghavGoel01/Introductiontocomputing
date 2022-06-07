318823f7da5ac4da7d62b2c345405a5cf71daf13
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-06-08 01:02:47 +0530

    Add files via upload

---
+++ assignment5_output_21102038.py
@@ -0,0 +1,147 @@
+PS D:\Python> python -u "d:\Python\assignment5_21102038.py"
+
+#Output of first question
+Enter a string :Raghav
+Reversed string is vahgaR
+
+#Output of second question
+Enter the range of numbers: 1 100
+Enter number: 7
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+77
+84
+91
+98
+
+#Output of third question
+Enter first side of the triangle: 3
+Enter second side of the triangle: 4
+Enter third side of the triangle: 5
+Area of the triangle is 6.0
+
+#Output of fourth question
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+#Output of fifth question
+Enter the no of rows: 5
+A
+BC
+DEF
+GHIJ
+KLMNO
+
+#Output of sixth question
+Enter the range of numbers: 1 200
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+103
+107
+109
+113
+127
+131
+137
+139
+149
+151
+157
+163
+167
+173
+179
+181
+191
+193
+197
+199
+
+#Output of seventh question
+77
+154
+231
+308
+385
+462
+
+#Output of eighth question
+Enter 1 number: 1
+Enter 2 number: 12
+Enter 3 number: 23
+Enter 4 number: -19
+Enter 5 number: 45
+Enter 6 number: 4
+Enter 7 number: -33
+Enter 8 number: 61
+Enter 9 number: 1
+Enter 10 number: -45
+[1, 12, 23, -19, 45, 4, -33, 61, 1, -45]
+Positive numbers are:
+1
+12
+23
+45
+4
+61
+1
+Negative numbers are:
+-19
+-33
+-45
+Odd numbers are:
+1
+23
+-19
+45
+-33
+61
+1
+-45
+Even numbers are:
+12
+4
+{1: 2, 12: 1, 23: 1, -19: 1, 45: 1, 4: 1, -33: 1, 61: 1, -45: 1}
+
+#Output of ninth question
+Enter a string: Sri Lanka is going through an unprecedented financial crisis
+{'Sri': 1, 'Lanka': 1, 'is': 1, 'going': 1, 'through': 1, 'an': 1, 'unprecedented': 1, 'financial': 1, 'crisis': 1}
