912e870dd86adbb3240dab588b2d3c4e41b1a516
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-06-08 00:25:08 +0530

    Add files via upload

---
+++ assignment4_output_21102038.py
@@ -0,0 +1,26 @@
+PS D:\Python> python -u "d:\Python\assignment4_21102038.py"
+Enter your marks: 67
+Your marks are 67 and corresponding grade is B
+Enter the year: 2000
+2000 is a leap year
+Question 1: 7*1 = 7
+Right!
+Question 2: 4*4 = 16
+Right!
+Question 3: 9*1 = 8
+Wrong. The answer is  9
+Question 4: 2*4 = 8
+Right!
+Question 5: 7*1 = 6
+Wrong. The answer is  7
+Question 6: 8*2 = 16
+Right!
+Question 7: 3*2 = 6
+Right!
+Question 8: 9*5 = 43
+Wrong. The answer is  45
+Question 9: 3*1 = 3
+Right!
+Question 10: 8*5 = 40
+Right!
+Number of candies in the box are 177
