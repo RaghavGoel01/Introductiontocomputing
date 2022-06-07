0366660844948f5f9985e44197a1ae3f3ebe6736
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-06-07 22:53:31 +0530

    Add files via upload

---
+++ assignment3_ouput_21102038.py
@@ -0,0 +1,55 @@
+PS D:\Python> python -u "d:\Python\assignment3_21102038.py"
+Enter a number: 12
+0b1100
+Please select operation -
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+
+Select operations form 1, 2, 3, 4 : 4
+Enter first number: 4
+Enter second number: 8
+4 / 8 = 0.5
+35
+Enter the value of n: 10
+45.0
+Enter the value of r: 4
+201.06192982974676
+Enter the value of a: 1
+Enter the value of b: 2
+2.115418060013892
+Enter value of x1: 3
+Enter value of x2: 4
+Enter value of y1: 1
+Enter value of y2: 5
+4.0
+0
+1
+2
+3
+4
+
+3
+4
+5
+6
+7
+8
+9
+
+4
+7
+10
+
+15
+13
+11
+9
+7
+
+
+Enter no of hydrogen atoms: 12
+Enter no of carbon atoms: 6
+Enter no of oxygen atoms: 6
+Molecular weight of the compound is  180.15588
