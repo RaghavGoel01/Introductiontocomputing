88fc16ce78a43c8f5f9b2a9554761e3683f5b05e
RaghavGoel01 <99827422+RaghavGoel01@users.noreply.github.com>
2022-06-08 00:28:31 +0530

    Update assignment4_output_21102038.py

--- assignment4_output_21102038.py
+++ assignment4_output_21102038.py
@@ -1,8 +1,14 @@
 PS D:\Python> python -u "d:\Python\assignment4_21102038.py"
+  
+#Output of first question
 Enter your marks: 67
 Your marks are 67 and corresponding grade is B
+
+#Output of second question
 Enter the year: 2000
 2000 is a leap year
+
+#Output of third question
 Question 1: 7*1 = 7
 Right!
 Question 2: 4*4 = 16
@@ -23,4 +29,6 @@ Question 9: 3*1 = 3
 Right!
 Question 10: 8*5 = 40
 Right!
+
+#Output of fourth question
 Number of candies in the box are 177
